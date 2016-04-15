1ee4ffbfea37bf7a5f6c55156c783a6b6c0e05a1
6apcyk <vba12@mail.ru>
2016-04-16 03:45:25 +0700

    Update sibnet-fastreply.meta.js

--- dist/sibnet-fastreply.meta.js
+++ dist/sibnet-fastreply.meta.js
@@ -7,9 +7,9 @@
 // @updateURL    https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/sibnet-fastreply.meta.js
 // @downloadURL  https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/sibnet-fastreply.user.js
 //
-// @version      0.4.4
+// @version      0.4.5
 // @require    	 http://code.jquery.com/jquery-2.2.0.min.js
-// @require    	 https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/text-editor.js?
+// @require    	 https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/text-editor.js
 // @require    	 https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/jscolor_mod.js
 // @include      http://forum.sibnet.ru/*
 // @match        http://forum.sibnet.ru/*
