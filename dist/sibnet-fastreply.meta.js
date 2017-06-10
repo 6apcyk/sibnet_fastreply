e480fb521868561b6a637d8d0b12a84fc5ee243b
6apcyk <vba12@mail.ru>
2017-06-10 18:35:19 +0700

    Create sibnet-fastreply.meta.js

---
+++ dist/sibnet-fastreply.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Sibnet FastReply Mod
+// @description  Модификация формы быстрого ответа на форуме сибнета (несколько кнопок-тегов и предпросмотр)
+// @namespace    -
+// @author       vba12
+//
+// @updateURL    https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/sibnet-fastreply.meta.js
+// @downloadURL  https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/sibnet-fastreply.user.js
+//
+// @version      0.5.0
+// @require    	 https://raw.githubusercontent.com/6apcyk/sibnet_fastreply/master/dist/jscolor_mod.js
+// @include      http://forum.sibnet.ru/*
+// @match        http://forum.sibnet.ru/*
+// @include      https://forum.sibnet.ru/*
+// @match        https://forum.sibnet.ru/*
+// @grant 		 none
+// ==/UserScript==
